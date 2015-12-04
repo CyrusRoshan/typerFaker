e7032690570d4222054710e52d59f4b880a74fcb
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2015-12-04 10:40:57 -0600

    cleaned up code (let commander do arg parsing, subbed === for == where needed, removed unnecessary global vars)

--- files/typerFaker.js
+++ files/typerFaker.js
@@ -1,112 +1,54 @@
+var program = require('commander');
 var Promise = require("bluebird");
 var robot = require("robotjs");
 
-//global argument values, parsed
-var selectedOptions = {
-	browser: true,
-	wpm: 125,
-	wait: 4000,
-	textGiven: false,
-	lazyBrowser: false,
-	maximumRealism: false,
-	argsError: false
-};
+main();
 
-readArgs();
-//if there are no errors. Otherwise, program will quit
-if(!selectedOptions.argsError){
-	//if not using browser
-	if(!selectedOptions.browser){
-		setTimeout(function(){typeItOut(0, selectedOptions.textGiven.length)}, selectedOptions.wait);
+function main(){
+	var programArgs = readArgs();
+	if(programArgs.nobrowser){
+		setTimeout(function(){typeItOut(programArgs)}, programArgs.wait);
 	}
-	//if user is using browser
-	else{
-
-	}
-}
-else{
-
 }
 
 
 function readArgs(){
-	//start at the 3rd argument
-	var currentArgPos = 2;
+	var programArgs = program;
+	programArgs
+		.version('0.0.1')
+		.usage('[options]')
+		.option('-b, --browser', 'Uses settings for browser, such as keypress activated auto script injection into current tab and keybindings for start, restart, pause, previous/next word navigation, and early exit')
+		.option('-n, --nobrowser', 'Uses settings for no browser (disables keybindings used for browser, and automatic script injection)')
+		.option('-w, --wpm <n>', 'Desired avg wpm to type at', parseInt)
+		.option('-p, --wait <n>', 'Used in addition to --nobrowser and --text. Sets time in ms to wait before automatically typing', parseInt)
+		.option('-t, --text [value]', 'Used in addition to --nobrowsesr and --wait. Supplies text to be inputted. Text should have double quotes around it, with interior double quotes prefixed with a backslash, e.g. -text "\"like this\", I say"')
+		.option('-l, --lazybrowser', 'Attempts to isolate the text needed to be typed, in addition to finding the correct time to begin typing')
+		.option('-m, --maximumrealism', 'Makes the typing as humanlike as possible. Adds simple typos and corrects them, types out each word letter by letter, with faster speeds on sequential letters, slower speeds on characters requiring shift, and in effect makes longer words take longer to type on average than shorter ones.')
+		.parse(process.argv);
 
-	//check if any args are added, if not, default values are declared on var initialization for selectedOptions anyway
-	while(currentArgPos < process.argv.length){
-		switch (currentArg()){
-			case "-browser":
-				currentArgPos++;
-				break;
+	return programArgs;
+}
 
-			case "-nobrowser":
-				currentArgPos++;
-				selectedOptions.browser = false;
+function typeItOut(programArgs){
+	//basically just go through and type all of the letters in a word at what would be the exact given wpm if calculation/comparison time was 0
+	var text = programArgs.text;
+	var wpm = programArgs.wpm;
+	var mr = programArgs.maximumrealism;
 
-			case "-wpm":
-				currentArgPos++;
-				if(currentArgPos < process.argv.length){
-					selectedOptions.wpm = currentArg();
-					currentArgPos++;
-				}
-				break;
-
-			case "-wait":
-				currentArgPos++;
-				if(currentArgPos < process.argv.length){
-					selectedOptions.wait = currentArg();
-					currentArgPos++;
-				}
-				break;
-
-			case "-text":
-				currentArgPos++;
-				selectedOptions.textGiven = process.argv[currentArgPos];
-				currentArgPos++;
-				break;
-
-			case "-lazybrowser":
-				selectedOptions.lazyBrowser = true;
-				currentArgPos++;
-				break;
-
-			case "-maximumrealism":
-				selectedOptions.maximumRealism = true;
-				currentArgPos++;
-				break;
-
-				//effectively exits program if an argument has a typo or is invalid
-			default:
-				selectedOptions.argsError = true;
-				console.log("Unknown option added, exiting program");
-				return;
+	//fix this function
+	function callTyper(){
+		if(i < length){
+			setTimeout(function(){
+				typeWord();
+			}, getSpeedOfVariedWPM(selectedOptions.wpm))
 		}
 	}
 
-	//function to get current arg
-	function currentArg(){
-		return process.argv[currentArgPos].toLowerCase();
-	}
-}
-
-function typeItOut(i, length){
-	var currentWord;
-	var j;
-	//basically just go through and type all of the letters in a word at what would be the exact given wpm if calculation/comparison time was 0
-
-	//fix this function
-	if(i < length){
-		setInterval(function(){
-			typeWord();
-		}, getSpeedOfVariedWPM(selectedOptions.wpm))
-	}
-
 	function typeWord(){
-		currentWord = selectedOptions.textGiven.substr(i).split(" ")[0];
-		for(j = 0; j < currentWord.length; j++){
+		var currentWord = selectedOptions.textGiven.substr(i).split(" ")[0];
+		for(var j = 0; j < currentWord.length; j++){
 			//so we don't type the backslash that comes before the double quotes, but is actually single quotes because of how the strings end up being stored
-			if(selectedOptions.textGiven[i] == "\\" && selectedOptions.textGiven[i+1] == "'"){
+			if(selectedOptions.textGiven[i] === "\\" && selectedOptions.textGiven[i+1] === "'"){
 				j++;
 			}
 			//print the current letter in the current word
